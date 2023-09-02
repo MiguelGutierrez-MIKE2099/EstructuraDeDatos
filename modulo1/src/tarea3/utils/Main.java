102e2e2c68ed89211601930185810e3a6cf6a93c
MiguelGutierrez-MIKE2099 <122950589+MiguelGutierrez-MIKE2099@users.noreply.github.com>
2023-09-01 21:40:53 -0600

    .

--- modulo1/src/tarea3/utils/Main.java
+++ modulo1/src/tarea3/utils/Main.java
@@ -1,11 +1,10 @@
-package tarea3.main;
+package tarea3.utils;
 
 import tarea3.ui.CLI;
+import tarea3.utils.*;
 
 public class Main {
     public static void main(String[] args) {
-
         CLI.launchApp();
-
     }
 }
